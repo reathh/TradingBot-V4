055099d8b3009c956ec52c1079316f323a963553
‏‏‎ ‎ <s@s.com>
2025-05-12 11:57:48 +0300

    Refactor TradingBotDbContextModelSnapshot to remove unused properties and add order status

---
+++ backend/TradingBot/Data/Migrations/20250512085409_RemovedOrdersData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradingBot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedOrdersData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Profit",
+                table: "Trades");
+
+            migrationBuilder.DropColumn(
+                name: "Canceled",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Closed",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Profit",
+                table: "Trades",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Canceled",
+                table: "Orders",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Closed",
+                table: "Orders",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
