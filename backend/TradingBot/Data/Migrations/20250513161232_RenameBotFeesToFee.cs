deb7aaf4d36b3fde46f97b08b47388f2f399d18f
‏‏‎ ‎ <s@s.com>
2025-05-13 19:14:14 +0300

    Rename Fees property to Fee in TradingBotDbContextModelSnapshot for consistency

---
+++ backend/TradingBot/Data/Migrations/20250513161232_RenameBotFeesToFee.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradingBot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameBotFeesToFee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Fees",
+                table: "Orders",
+                newName: "Fee");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Fee",
+                table: "Orders",
+                newName: "Fees");
+        }
+    }
+}
