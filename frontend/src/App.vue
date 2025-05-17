7505bec71a5d402e01cb00c6417a0f9e7ecfd619
‏‏‎ ‎ <s@s.com>
2025-05-17 19:44:36 +0300

    Remove unused global styles from App.vue for cleaner codebase

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,24 +15,3 @@ onMounted(() => {
   // Theme is automatically initialized when store is created
 });
 </script>
-
-<style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-  font-family: "Poppins", sans-serif;
-  background-color: #1e1e2f;
-  color: #ffffff;
-}
-
-body.white-content {
-  background-color: #f5f6fa;
-  color: #525f7f;
-}
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-weight: 400;
-}
-</style>
